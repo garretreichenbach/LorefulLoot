2df1cacb38c06dc4cb8e43b7e1765a173425ea51
Garret Reichenbach <garretreichenbach@gmail.com>
2026-03-25 12:10:44 -0700

    Delete Test.java

--- src/main/java/videogoose/lorefulloot/Test.java
+++
@@ -1,8 +0,0 @@
-package videogoose.lorefulloot;
-import org.schema.game.common.data.world.StellarSystem;
-import org.schema.common.util.linAlg.Vector3i;
-public class Test {
-    public static void test(StellarSystem system) {
-        Vector3i local = system.getLocalPos(new Vector3i());
-    }
-}
